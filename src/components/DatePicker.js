5797dc4dde431ab9a05cb895df0633e70284fca3
began91 <began91@yahoo.com>
2022-06-22 23:52:03 -0400

    Added full week selection

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -24,20 +24,52 @@ export default function DatePicker(props) {
 
     const currentYear = new Date().getFullYear();
 
+    let week = [];
+    week[0] = new Date(date);
+    week[0].setDate(date.getDate()-date.getDay());
+    for (let i=1;i<=6;i++) {
+        week[i] = new Date(date);
+        week[i].setDate(date.getDate()-date.getDay() + i)
+    }
+    const daysInMonth1 = week.filter(day=>day.getMonth()===week[0].getMonth()).length
+    const daysInMonth2 = 7 - daysInMonth1;
+    const styleMonth1 = {
+        flex: daysInMonth1 + ' 1 0'
+    };
+    const styleMonth2 = {
+        flex: daysInMonth2+ ' 1 0'
+    };
+
     return (
         <div id="DatePicker">
             <div id="dateRow">
-                <button id='backMonth' value={-1} onClick={changeMonth}>bb</button>
-            <button id='backDay' value={-1} onClick={changeDay}>b</button>
+                <button id='backMonth' value={-1} onClick={changeMonth}>&lt;&lt;</button>
+            <button id='backDay' value={-1} onClick={changeDay}>&lt;</button>
             <div id="dateSquare" onClick={()=>setDisplayCalendar(true)}>
-                <div id="month">
+                {week.every(day=>day.getMonth()===date.getMonth()) ? 
+                (<div className="month" id="month">
                     {date.toLocaleString(undefined,{month:'short'})}
+                </div>):
+                (<div id="monthRow">
+                    <div className="month" id="month1" style={styleMonth1}>
+                        {week[0].toLocaleString(undefined,{month:'short'})}
+                    </div>
+                    <div className="month" id="month2" style={styleMonth2}>
+                        {week[6].toLocaleString(undefined,{month:'short'})}
+                    </div>
+                </div>)}
+                
+                <div id="week">
+                    {week.map(day=>(
+                        <DaySquare key={day.getDay()} date={day} />
+                    ))}
                 </div>
-                <DaySquare date={date} />
+                
+                {/* <DaySquare date={date} /> */}
                 
             </div>
-            <button id='nextDay' value={1} onClick={changeDay}>f</button>
-            <button id='nextMonth' value={1} onClick={changeMonth}>ff</button>
+            <button id='nextDay' value={1} onClick={changeDay}>&gt;</button>
+            <button id='nextMonth' value={1} onClick={changeMonth}>&gt;&gt;</button>
             </div>
             
             <br/>
