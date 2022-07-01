9d7cdc51ca147977c2cb4ca9add466136bbe4033
began91 <began91@yahoo.com>
2022-07-01 00:28:34 -0400

    Added first counting timer

--- src/components/WorkoutScreen.js
+++ src/components/WorkoutScreen.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React ,{ useState }from 'react';
 
 export default function WorkoutScreen (props) {
+    const workout = props.workout;
+    const [totalT,setTotalT] = useState(0);
+    const totalTime = setInterval(updateActiveTimers,1000);
+
+    function stopAllTimers() {
+        clearInterval(totalTime);
+    }
+
+    function updateActiveTimers() {
+        setTotalT(totalT+1)
+    }
+
+    const endWorkout = () => {
+        stopAllTimers()
+        props.setWorkoutStatus(false)
+    }
+    
     return (
         <div>
             Workout Screen
-            <button onClick={()=>props.setWorkoutStatus(false)}>Stop Workout</button>
+            <button onClick={endWorkout}>Stop Workout</button>
+            {totalT}
         </div>
     )
 }
